1b321c512b64d20cefff384ed7faf1cd0288a982
yogendrabaskota <yogendrabaskota18@gmail.com>
2025-06-17 12:32:19 +0545

    backend completed and ejs setup

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
+import path from 'path'
 import ('../src/controllers/todoController')
 
 const app = express()
+app.set("view engine","ejs")
 
-
+app.set("views",path.join(__dirname,'views'))
+app.get("/",(req:Request,res:Response)=>{
+    res.render("home.ejs")
+})
 
 
 
